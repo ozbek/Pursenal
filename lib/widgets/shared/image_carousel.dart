ec78fd5cb36eadf3e7449496c59e890c058d20b8
Kaashi-r <kaashier@gmail.com>
2025-09-21 12:33:44 +0530

    improved missing photo placeholder

--- lib/widgets/shared/image_carousel.dart
+++ lib/widgets/shared/image_carousel.dart
@@ -24,69 +24,82 @@ class ImageCarousel extends StatelessWidget {
           spacing: 8,
           mainAxisAlignment: MainAxisAlignment.center,
           children: filePaths.mapIndexed((index, filePath) {
-            return Material(
-              elevation: 3,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(0),
-                child: GestureDetector(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => Stack(
-                        children: [
-                          Dialog(
-                            backgroundColor: Colors.transparent,
-                            shape: Border.all(),
-                            child: SizedBox(
-                              child: Image.file(
-                                errorBuilder: (context, error, stackTrace) =>
-                                    const Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text("Media error"),
-                                  ),
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: GestureDetector(
+                onTap: () {
+                  if (!File(filePath).existsSync()) return;
+                  showDialog(
+                    context: context,
+                    builder: (context) => Stack(
+                      children: [
+                        Dialog(
+                          backgroundColor: Colors.transparent,
+                          shape: Border.all(),
+                          child: SizedBox(
+                            child: Image.file(
+                              errorBuilder: (context, error, stackTrace) =>
+                                  const Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text("Media error"),
                                 ),
-                                File(filePath),
-                                fit: BoxFit.fitWidth,
+                              ),
+                              File(filePath),
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: -5,
+                          right: -5,
+                          child: Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.close,
+                                color: Colors.red,
+                                size: 30,
                               ),
                             ),
                           ),
-                          Positioned(
-                            top: -5,
-                            right: -5,
-                            child: Padding(
-                              padding: const EdgeInsets.all(24.0),
-                              child: IconButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                icon: const Icon(
-                                  Icons.close,
-                                  color: Colors.red,
-                                  size: 30,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(maxHeight: maxHeight),
-                    child: Image.file(
-                      errorBuilder: (context, error, stackTrace) =>
-                          const Center(
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: maxHeight),
+                  child: Image.file(
+                    errorBuilder: (context, error, stackTrace) =>
+                        const AspectRatio(
+                      aspectRatio: .5,
+                      child: Card(
                         child: Padding(
                           padding: EdgeInsets.all(8.0),
-                          child: Text("Media error"),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.broken_image),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  "Media error",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      File(filePath),
-                      fit: BoxFit.cover,
                     ),
-                  ).animate(delay: (index * 50).ms).fade(duration: 250.ms),
-                ),
+                    File(filePath),
+                    fit: BoxFit.cover,
+                  ),
+                ).animate(delay: (index * 50).ms).fade(duration: 250.ms),
               ),
             );
           }).toList(),
